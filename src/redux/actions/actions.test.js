17193353d5b4d1ad09949ffc0213016b01214fcc
JZachrisson <jesper.zachrisson@gmail.com>
2021-01-05 15:08:38 +0100

    adding actions

---
+++ src/redux/actions/actions.test.js
@@ -0,0 +1,16 @@
+import actions from '.';
+import types from '../constants';
+
+describe('Actions', () => {
+  const todoText = 'A todo';
+
+  it('Should create an action to add a todo', () => {
+    const expectedAction = {
+      type: types.SUBMIT_TODO,
+      id: 1,
+      text: todoText,
+    };
+
+    expect(actions.submitTodo(todoText)).toEqual(expectedAction);
+  });
+});
